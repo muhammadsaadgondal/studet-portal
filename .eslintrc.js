3fc908e3dd1c425db741b55a958a4b177a3955dc
saadjutt10 <saadgondal203@gmail.com>
2023-12-19 20:56:42 +0500

    linter identation update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,14 @@
 module.exports = {
     extends: [
-      'react-app',
-      'react-app/jest',
-      'plugin:jest-dom/recommended',
-    ],
+        "stylelint-config-recommended",
+        "stylelint-config-recommended-scss",
+        "stylelint-config-prettier",
+      ],
     // Add any additional rules or overrides here
     rules: {
-      'no-sequences': 'off',  // Disable the no-sequences rule
-      'no-restricted-globals': ['error', 'window'],  // Only allow the 'window' global
-    },
+        "block-opening-brace-space-before": null,
+        "indentation": 2,
+        // Add or modify other rules as needed
+      },
   };
   
